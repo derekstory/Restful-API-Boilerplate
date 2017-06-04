d2eed5381329a984918ffb0272cdf814703c0425
Derek Story <derek.a.story@gmail.com>
2017-06-04 04:01:04 -0400

    Nix unused helper.

--- views/helpers.js
+++ views/helpers.js
@@ -42,20 +42,4 @@ $(function() {
 		});
 	};
 
-	// PUT request
-	$.put = function(url, data, callback, type){
-		if ( $.isFunction(data) ){
-			type = type || callback;
-			callback = data;
-			data = {};
-		}
-		return $.ajax({
-			url: url,
-			type: 'PUT',
-			success: callback,
-			data: data,
-			contentType: type
-		});
-	};
-
 });
